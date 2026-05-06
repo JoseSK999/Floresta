429d5cecff6b024b8c4a0c79cb6cedf56caf39b5
jaoleal <jgleal@protonmail.com>
2026-05-05 23:12:41 -0300

    chore(flake): remove unecessary inputs, move flake framework to flake-parts and add nix-fmt and statix check
    Those removed inputs were unecessary, nixpkgs-unstable is not more needed because the removal of the hooks and rust-overlay were also not used, we already exports rustup
    
    Now nix flake check command is executed in just pcc but only when nix is available and only when flake is altered so nix doesnt bother CI
    
    This commit also removes florest-flake since this input is focused on packaging and this is a development flake floresta-flake.
    
    This commit also introduces some new packages so the flake is extensive covering everything that one developing in floresta might need

--- flake.nix
+++ flake.nix
@@ -1,82 +1,60 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    floresta-flake.url = "github:getfloresta/floresta-nix/stable_building";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
-      rust-overlay,
-      flake-utils,
-      nixpkgs-unstable,
-      floresta-flake,
+      flake-parts,
+      treefmt-nix,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
 
-        pkgs = import nixpkgs { inherit system overlays; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        # We use src a lot across this flake
-        src = ./.;
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.statix.enable = true;
+          };
 
-        inherit (floresta-flake.lib.${system}) florestaBuild;
-      in
-      with pkgs;
-      {
-        packages = {
-          florestad = florestaBuild.build {
-            inherit src;
-            packageName = "florestad";
-          };
-          floresta-cli = florestaBuild.build {
-            inherit src;
-            packageName = "floresta-cli";
-          };
-          libfloresta = florestaBuild.build {
-            inherit src;
-            packageName = "libfloresta";
-          };
-          floresta-debug = florestaBuild.build {
-            inherit src;
-            packageName = "floresta-debug";
-          };
-          default = florestaBuild.build {
-            inherit src;
-            packageName = "all";
-          };
+          devShells.default =
+            let
+              packages = with pkgs; [
+                just
+                rustup
+                git
+                boost
+                cmake
+                typos
+                python312
+                uv
+                gcc
+                go
+                cargo-hack
+                pkg-config
+                openssl
+                llvmPackages.clang
+              ];
+            in
+            pkgs.mkShell {
+              inherit packages;
+              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+              CMAKE_PREFIX_PATH = "${pkgs.boost.dev}";
+            };
         };
-        devShells.default =
-          let
-            # This is the dev tools used while developing in Floresta.
-            packages = with pkgs; [
-              just
-              rustup
-              git
-              boost
-              cmake
-              typos
-              python312
-              uv
-              gcc
-              go
-              cargo-hack
-            ];
-          in
-          mkShell {
-            inherit packages;
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-            CMAKE_PREFIX_PATH = "${pkgs.boost.dev}";
-          };
-      }
-    );
+    };
 }
