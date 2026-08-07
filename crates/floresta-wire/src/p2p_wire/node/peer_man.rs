c237aca0dbf51fae1a179a38d827f4c663f2782e
Davidson Souza <me@dlsouza.lol>
2026-08-07 14:41:48 -0300

    fix: check receiving addresses timestamp
    Bitcoin Core checks AddrV2 messages' timestamp, and prevents them from being
    too far into the future. For us, if they are too into the future, they would
    take the top of our address manager and not be evicted, even if it's just
    boggus addresses created by an attacker.
    
    This commit fixes that by dropping any address who's timestamp is more
    than ten minutes into the future. I'm not disconnecting the peer, as some
    clock drift might cause us to perceive that for some addresses.
    
    Credit for this finding goes to the bitcoinfuzz project that found it while
    differential testing floresta's AddrV2 code with Bitcoin Core's.

--- crates/floresta-wire/src/p2p_wire/node/peer_man.rs
+++ crates/floresta-wire/src/p2p_wire/node/peer_man.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::time::Duration;
 use std::time::Instant;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
@@ -661,10 +662,25 @@ where
         peer: u32,
         addresses: Vec<AddrV2Message>,
     ) -> Result<(), WireError> {
-        self.inflight.remove(&InflightRequests::GetAddresses);
+        /// Don't allow addresses whose timestamp are more than 10 minutes into the future, or an
+        /// attacker could make us drop all addresses from our address manager with their own.
+        const ADDRESS_TIME_CUTOFF: Duration = Duration::from_secs(600); // 10 minutes
+
         debug!("Got {} addresses from peer {}", addresses.len(), peer);
-        let addresses: Vec<_> = addresses.into_iter().map(|addr| addr.into()).collect();
-        self.address_man.push_addresses(&addresses);
+        self.inflight.remove(&InflightRequests::GetAddresses);
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("System clock isn't working");
+
+        let cutoff = now + ADDRESS_TIME_CUTOFF;
+        let good_addresses: Vec<_> = addresses
+            .into_iter()
+            .filter(|addr| u64::from(addr.time) < cutoff.as_secs())
+            .map(|addr| addr.into())
+            .collect();
+
+        self.address_man.push_addresses(&good_addresses);
 
         // For feeler peers, we ask for addresses to populate our address manager,
         // then disconnect them
